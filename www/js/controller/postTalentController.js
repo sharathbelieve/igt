fafbe107e5be8163988e71bb3fd6ea713214a729
sharathbelieve <sharathbelieve@users.noreply.github.com>
2015-06-18 22:56:18 +0530

    Update postTalentController.js

--- www/js/controller/postTalentController.js
+++ www/js/controller/postTalentController.js
@@ -2,21 +2,44 @@ var PostTalentController = angular.module('PostTalentController', []);
 
 PostTalentController.controller('PostTalentController', function($scope, $stateParams, $http, domainUrl, $ionicModal) {
 	
+  
+$scope.selectCat = 'Select Categories';
+$scope.selSubCat = 'Select Sub Category';
+
   var cid;
   $http.get(domainUrl+'categories').success(function(categories){
   console.log(categories);
   $scope.categories = categories;
   }, function(err){
-
+  	console.log(err);
   })
   
-  $http.get(domainUrl+'subcategories?cid=1').success(function(subCategories){
+  
+  
+var selectedCateogryID;
+var selectedSubCateogryID;
+  $scope.getCID = function() {
+  
+  	console.log(this.category.cid);
+    $scope.selectCat = this.category.name;
+    selectedCateogryID = this.category.cid;
+
+    $http.get(domainUrl+'subcategories?cid=' + this.category.cid).success(function(subCategories){
   console.log(subCategories);
   $scope.subCategories = subCategories;
   }, function(err){
 
   })
+  }
+
+  $scope.getSubCID = function() {
   
+  	
+    $scope.selSubCat = this.subCategory.name;
+    selectedSubCateogryID = this.subCategory.cid;
+  }
+
+
 	$ionicModal.fromTemplateUrl('templates/catModal.html', function (modal) {
 	$scope.catModal = modal;
 	},{
@@ -33,14 +56,23 @@ PostTalentController.controller('PostTalentController', function($scope, $stateP
 		backdropClickToClose: false
 	});
 
+
+	$scope.doneCat = function() {
+		$scope.catModal.hide();
+	}
+
+	$scope.doneSubCat = function() {
+		$scope.subCatModal.hide();
+	}
+
   $scope.videoURL = false;
   $scope.submit = function() {
     alert(this.talent);
     alert(this.description);
     alert(this.choice);
     alert(this.category);
-    alert(this.item.name);
-    alert(this.dd.name);
+    alert(selectedCateogryID);
+    alert(selectedSubCateogryID);
   }
   
   $scope.cat = function() {
